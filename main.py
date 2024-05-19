b9d09c8e4fc870a84a72b0725c6e81cd3cd31e83
Antonio Claudio <claudio1419teixeira2020@gmail.com>
2024-05-19 12:39:17 -0300

    fix: now it is possible to create a zip

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, send_file
+from flask import Flask, request, send_file, jsonify
 from pikepdf import Pdf, PdfImage
 import zipfile
 import os
@@ -6,36 +6,39 @@ import io
 
 app = Flask(__name__)
 
+@app.route('/')
+def home():
+    return "Server running..."
+    
 @app.route('/upload-pdf', methods=['POST'])
 def upload_pdf():
     if 'pdf' not in request.files:
-        return 'Nenhum arquivo enviado!', 400
+        return jsonify({"error": "Nenhum arquivo enviado!"}), 400
     
     pdf_file = request.files['pdf']
 
     if pdf_file.filename == '':
-        return 'Nenhum arquivo selecionado!', 400
+        return jsonify({"error": "Nenhum arquivo selecionado!"}), 400
     
     pdf_file.save('temp.pdf')
 
     pdf = Pdf.open('temp.pdf')
-    pdf_image = []
+    pdf_images = []
     images = []
     pdf_images_not_duplicate = []
 
-    for page in enumerate(pdf.pages):
-        for raw_image in page.images.items():
-            pdf_image.append(PdfImage(raw_image))
+    for page in pdf.pages:
+        for name, raw_image in page.images.items():
+            pdf_images.append(PdfImage(raw_image))  
 
-    for image in pdf_image:
+    for image in pdf_images:
         if image not in pdf_images_not_duplicate:
             pdf_images_not_duplicate.append(image)
 
     indice = 1
     for image in pdf_images_not_duplicate:
-        image_filename = f'image_{indice}'
-        image.extract_to(fileprefix=image_filename)
-        images.append(image_filename)
+        image_filename = f'images/image_{indice}'
+        images.append(image.extract_to(fileprefix=image_filename))
         indice += 1
 
     zip_buffer = io.BytesIO()
@@ -47,7 +50,7 @@ def upload_pdf():
     zip_buffer.seek(0)
     os.remove('temp.pdf')
 
-    return send_file(zip_buffer, mimetype='application/zip', as_attachment=True, attachment_filename='images.zip')
+    return send_file(zip_buffer, mimetype='application/zip', as_attachment=True, download_name='images.zip')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0')
